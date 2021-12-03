e85a8e172b8fa1e799d255dc60b9f500146e3bb2
Charlotte Vermandel <charlottevermandel@gmail.com>
2021-12-03 15:08:12 +0100

    Add day 2 part 1

--- src/day_02/part_1/aoc.ts
+++ src/day_02/part_1/aoc.ts
@@ -1,3 +1,22 @@
+type instruction = [string, number]
+type instructions = instruction[]
+
+function parseInput(input: string): instructions {
+  return input.split("\n").filter(str => str.length > 0).map(raw => {
+    let [dir, dis] = raw.split(" ")
+    return [dir[0], parseInt(dis)]
+  })
+}
+
 export function main(input: string): number {
-  return 1;
+  const instructions = parseInput(input)
+  
+  const [ hor, depths ] = instructions.reduce(([hor, depths], [instruction, steps]) => {
+    if (instruction === 'f') return [ hor += steps, depths ]
+    else if (instruction === 'u') return [ hor, depths += (steps * -1) ]
+    return [ hor, depths += steps ]
+  }, [0,0])
+
+  
+  return hor * depths;
 }
