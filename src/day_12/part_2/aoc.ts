4d30b5736d6f43955dcd6871f9a517ca09f26958
Charlotte Vermandel <charlottevermandel@gmail.com>
2021-12-13 03:59:59 +0100

    Add day 12 part 2

--- src/day_12/part_2/aoc.ts
+++ src/day_12/part_2/aoc.ts
@@ -1,3 +1,61 @@
+import { isLowerCase } from '../../../utils/index.ts';
+
+type Node = {
+  name: string;
+  links: Node[];
+  small: boolean;
+};
+
+type Links = {
+  [name: string]: Node;
+};
+
+function createNode(name: string) {
+  return {
+    name,
+    links: [],
+    small: isLowerCase(name),
+  };
+}
+
+function parseInput(input: string) {
+  const lines = input
+    .split('\n').filter((l) => l.length > 1);
+  const links: Links = {};
+
+  lines.forEach((line) => {
+    const [left, right]: string[] = line.split('-');
+
+    if (!links[left]) links[left] = createNode(left);
+    if (!links[right]) links[right] = createNode(right);
+
+    if (left !== 'start') links[right].links.push(links[left]);
+    if (right !== 'start') links[left].links.push(links[right]);
+  });
+  return links;
+}
+
+function visitCave(node: Node, path: string, twice: boolean): string[] {
+  path = path + `,` + node.name;
+  let paths: string[] = [];
+  if (node.name === 'end') return [path];
+  node.links.forEach((childNode) => {
+    if (childNode.name === 'start') console.log(node);
+    if (!(childNode.small && path.includes(childNode.name))) {
+      paths.push(...visitCave(childNode, path, twice));
+    } else if (childNode.small && path.includes(childNode.name) && !twice) {
+      paths.push(...visitCave(childNode, path, true));
+    }
+  });
+
+  return paths;
+}
+
 export function main(input: string): number {
-  return 1;
+  const labyrinth = parseInput(input);
+  const paths: string[] = [];
+  for (const node of labyrinth['start'].links) {
+    paths.push(...visitCave(node, 'start', false));
+  }
+  return paths.length;
 }
