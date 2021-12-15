cca4b72b5cee42ee3a337d2a7af9a682a29964bd
Charlotte Vermandel <charlottevermandel@gmail.com>
2021-12-15 22:02:30 +0100

    Add day 15 part 1

--- src/day_15/part_1/aoc.ts
+++ src/day_15/part_1/aoc.ts
@@ -1,87 +1,60 @@
-function adjacentPoints(y: number, x: number, map: number[][]): number[][] {
-  const height = map.length;
-  const width = map[0].length;
-  const positions = [];
-
-  // if (x == 0) positions.push([y, x + 1]);
-  // else if (x == width - 1) positions.push([y, x - 1]);
-  // else positions.push([y, x - 1], [y, x + 1]);
-
-  // if (y == 0) positions.push([y + 1, x]);
-  // else if (y == height - 1) positions.push([y - 1, x]);
-  // else positions.push([y + -1, x], [y + 1, x]);
-
-  // if (x == 0) positions.push([y, x + 1]);
-  if (x !== width - 1) positions.push([y, x + 1]);
-  // else positions.push([y, x - 1], [y, x + 1]);
-
-  // if (y == 0) positions.push([y + 1, x]);
-  if (y !== height - 1) positions.push([y + 1, x]);
-  // else positions.push([y + -1, x], [y + 1, x]);
-
-  return positions;
-}
-
 function parseInput(input: string): number[][] {
   const i = input.split('\n').map((line) =>
     line.split('').map((x) => parseInt(x))
   );
-
   i.pop();
   return i;
 }
 
-export function main(input: string): number {
-  const cave = parseInput(input);
+function adjacentInf(y: number, x: number, map: number[][]): number[][] {
+  const height = map.length;
+  const width = map[0].length;
+  const positions = [];
 
-  const cache: string[] = [`${0},${0}`];
-  const adjacents = crawlCave(0, 0, cave, 0, cache);
+  if (x !== 0 && x !== width - 1) {
+    if (map[y][x - 1] === Infinity) positions.push([y, x - 1]);
+    if (map[y][x + 1] === Infinity) positions.push([y, x + 1]);
+  } else if (x === 0 && map[y][x + 1] === Infinity) positions.push([y, x + 1]);
+  else if (x === width - 1 && map[y][x + -1] === Infinity) {
+    positions.push([y, x - 1]);
+  }
 
-  console.log(adjacents);
-  return 1;
+  if (y !== 0 && y !== height - 1) {
+    if (map[y - 1][x] === Infinity) positions.push([y - 1, x]);
+    if (map[y + 1][x] === Infinity) positions.push([y + 1, x]);
+  } else if (y === 0 && map[y + 1][x] === Infinity) positions.push([y + 1, x]);
+  else if (y === height - 1 && map[y - 1][x] === Infinity) {
+    positions.push([y - 1, x]);
+  }
+  return positions;
 }
 
-let globalRisk = 0;
+export function main(input: string): number {
+  const cave = parseInput(input);
+  const fog = [...cave].map((line) => line.map((x) => Infinity));
+  const stack: number[][] = [[0, 0]];
+
+  fog[0][0] = cave[0][0];
+  const value = crawlCave(cave, fog, stack);
+  return value - cave[0][0];
+}
 
 function crawlCave(
-  y: number,
-  x: number,
   cave: number[][],
-  risk: number,
-  cache: string[],
+  fog: number[][],
+  stack: number[][],
 ): number {
-  // console.log(`--------`);
-  // console.log({ curr: `${y},${x}`, risk, cache });
-  risk += cave[y][x];
-  if (globalRisk !== 0 && risk >= globalRisk) {
-    return globalRisk;
-  }
-  if (x === cave[0].length - 1 && y === cave.length - 1) {
-    if (globalRisk === 0) globalRisk = risk;
-    else if (globalRisk !== 0 && risk < globalRisk) {
-      globalRisk = risk;
+  while (true) {
+    const [y, x] = stack.shift() || [0, 0];
+    const curr = fog[y][x];
+    if (y === cave.length - 1 && x === cave[0].length - 1) {
+      return curr;
     }
-    console.log(risk);
-    return risk;
+    const infinites = adjacentInf(y, x, fog);
+    infinites.forEach(([y1, x1]) => {
+      fog[y1][x1] = cave[y1][x1] + curr;
+    });
+    stack.push(...infinites);
+    stack.sort(([y1, x1], [y2, x2]) => fog[y1][x1] - fog[y2][x2]);
   }
-  const adjacents = adjacentPoints(y, x, cave).sort(([y1, x1], [y2, x2]) =>
-    cave[y1][x1] - cave[y2][x2]
-  );
-
-  const risksFilter = adjacents.filter(([y1, x1]) =>
-    !cache.includes(`${y1},${x1}`)
-  );
-  if (risksFilter.length === 0) {
-    return 0;
-  }
-  risksFilter.forEach(([y1, x1]) => cache.push(`${y1},${x1}`));
-
-  const risks = risksFilter.map(([y1, x1]) => {
-    const newCache = [...cache];
-    return crawlCave(y1, x1, cave, risk, newCache);
-  }).filter((risk) => risk !== 0);
-  // console.log({ risks });
-  // console.log(`--------`);
-  if (risks.length === 0) return 0;
-  return Math.min(...risks);
 }
